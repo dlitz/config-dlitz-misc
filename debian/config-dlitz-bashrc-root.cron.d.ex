8c87d50e4d76f112b88bd41afbe0431c979a63ff
Darsey Litzenberger <dlitz@dlitz.net>
2023-10-24 17:49:18 -0700

    Remove dh_make boilerplate

--- debian/config-dlitz-bashrc-root.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the config-dlitz-bashrc-root package.
-#
-0 4	* * *	root	[ -x /usr/bin/config-dlitz-bashrc-root_maintenance ] && /usr/bin/config-dlitz-bashrc-root_maintenance
